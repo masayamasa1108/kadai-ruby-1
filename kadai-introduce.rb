c6e0fbe02659691148f5add756ccae5831a4ff2f
masayamasa1108 <masayamasa1108@gmail.com>
2021-09-11 13:21:46 +0000

    delete variable : result

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -2,7 +2,4 @@ lastname = "雅文"
 familyname = "山本"
 age = 36
 
-# name and age
-result = familyname + lastname + "です。" + age.to_s + "歳です。"
-
-puts result
+puts familyname + lastname + "です。" + age.to_s + "歳です。"
